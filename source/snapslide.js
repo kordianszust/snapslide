122821593fbe9d9efaf88c290dd2b213fd188fa9
kordianszust <kordianszust@gmail.com>
2019-12-30 15:45:32 +0100

    Active slide
    Added active slide with animations

--- source/snapslide.js
+++ source/snapslide.js
@@ -1,14 +1,32 @@
 var nunumR
-$('.sec').html('Active slide: ' + 1);
+$(document).ready(function() {
+    $('.sec').html('Active slide: ' + 1);
+    $("section:nth-child(1)").addClass('active');
+    $("li:nth-child(1)").addClass('active');
+});
 $(".snap-container").scroll(function() {
+
+
+
     $('.text').html('ScrollTop: ' + $(".snap-container").scrollTop());
     nunum = (($(".snap-container").scrollTop() / $('.snap-container').height()) + 1);
     nunumR = nunum.toString().split(".");
+
+
+    $("section").removeClass('active');
+    $("section:nth-child(" + nunumR[0] + ")").addClass('active');
+
+
     $('.sec').html('Active slide: ' + (nunumR[0]));
     clearTimeout($.data(this, 'scrollTimer'));
     $.data(this, 'scrollTimer', setTimeout(function() {
+
+
+        // rewrite for function
+
         $("li").removeClass('active');
         $("li:nth-child(" + nunumR[0] + ")").addClass('active');
+
     }, 150));
 });
 $(window).resize(function() {
@@ -23,5 +41,7 @@ $(window).resize(function() {
 $('.count').html('Slides: ' + $(".snap-container section").length);
 // $('.sizeofone').html(($('.snap-container').height()) + ' - ' + ($('.snap-container').height() * $(".snap-container section").length));
 
-$(".goto").click(function() {});
-console.log(1);
+$("li").click(function() {
+	console.log((($(this).attr('data-slide-link')) * $('section').height()));
+    $(".snap-container").animate({ scrollTop: ((($(this).attr('data-slide-link')) * $('section').height()) + (60 * ($(this).attr('data-slide-link') - 1)) }, 1500);
+});
